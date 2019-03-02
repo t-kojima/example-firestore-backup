91a6ff2e1baf053f9568b046e008c61c221b3ad7
t-kojima <t-kojima@sonicgarden.jp>
2019-03-02 16:00:33 +0900

    clone: project

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+// Copyright 2017 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+const functions = require('firebase-functions');
+
+exports.hourly_job = functions.pubsub
+  .topic('hourly-tick')
+  .onPublish((message) => {
+    console.log("This job is run every hour!");
+    if (message.data) {
+      const dataString = Buffer.from(message.data, 'base64').toString();
+      console.log(`Message Data: ${dataString}`);
+    }
+
+    return true;
+  });
